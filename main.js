bfcf0e1d132ec74926aa361e6d52ab582224779c
Francesconliner <francescodivincenzo95@outlook.it>
2020-11-23 16:14:15 +0100

    Bonus2

--- main.js
+++ main.js
@@ -2,21 +2,35 @@ var app = new Vue({
     el: '#root',
     data: {
         discs: [],
+        order_discs: [],
+        is_order: false,
         categories: 'All',
         },
     methods: {
         yearOrder: function(){
-            // this.discs.sort(this.discs[].year);
-            for (i = 0; i < this.discs.length; i++) {
-                for (j = 0; j < this.discs.length; j++) {
-                    if (this.discs[j].year > this.discs[j + 1].year) {
-                        let tmp = this.discs[j];
-                        this.discs[j] = this.discs[j + 1];
-                        this.discs[j + 1] = tmp;
+            /*Clono l'array discs attraverso la funzione map*/
+            this.order_discs = this.discs.map((element, index) => {
+                return element;
+            });
+
+            /*eseguo l'ordinamento all'interno del nuovo array*/
+            /*mi creo ua struttura bidimensionale che mi permette di confrontare ogni singolo anno con l'anno presente nell'emento array successivo*/
+            for (i = 0; i < this.order_discs.length; i++) {
+                for (j = 0; j < this.order_discs.length; j++) {
+                    /*se l'elemento successivo non esitiste passa avanti*/
+                    if(this.order_discs[j+1]!=undefined){
+                        /*se il confronto di anno è vero mi sostituisce di posizione i due elementi all'interno dell'array*/
+                        if (this.order_discs[j].year > this.order_discs[j + 1].year) {
+                            let tmp = this.order_discs[j];
+                            this.order_discs[j] = this.order_discs[j + 1];
+                            this.order_discs[j + 1] = tmp;
+                            console.log("fatto");
+                        }
                     }
                 }
             }
-            console.log(this.discs);
+            /*ogni volta che premo il pulsante mi inverte lo stato da orinato a non ordinato, mostrandomi il relativo array*/
+            this.is_order=!this.is_order;
         },
     },
 
@@ -27,6 +41,8 @@ var app = new Vue({
                     console.log(risposta);
                     app.discs=risposta.data.response;
                 });
+
+
     },
 
 })
